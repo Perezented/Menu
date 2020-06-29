e44a2a2c8ab0a4139e58e376adc996bc1c53c3a3
Michael Perez <mperez6473@gmail.com>
2020-06-29 17:06:43 -0500

    Added cors and helmet

--- server.js
+++ server.js
@@ -1,29 +1,20 @@
 const express = require("express");
-
+const helmet = require("helmet");
+const cors = require("cors");
 var morgan = require("morgan");
 const server = express();
 
-// const seafoodRouter = require("./seafoodRouter/seafoodRouter");
-// const steaksRouter = require("./steaksRouter/steaksRouter");
-// const chickenRouter = require("./chickenRouter/chickenRouter");
-// const soupsRouter = require("./soupsRouter/soupsRouter");
-// const childsPlatesRouter = require("./childsPlatesRouter/childsPlatesRouter");
+const menuItems = require("./menuItems");
 
-const allRouter = require("./menuItems");
 server.use(morgan("combined"));
 server.use(express.json());
+server.use("/menu", menuItems);
+
 server.get("/", (req, res) => {
     res.status(200).json({
         Message: "Welcome to the home slash of the menu server.",
         menuItems,
     });
 });
-
-server.use("/menu", allRouter);
-// server.use("/api/seafood", seafoodRouter);
-// server.use("/api/steaks", steaksRouter);
-// server.use("/api/chicken", chickenRouter);
-// server.use("/api/soups", soupsRouter);
-// server.use("/api/childsPlates", childsPlatesRouter);
 
 module.exports = server;
