60119bb474e51f0e3819869f9b3f53ae1ce4d86b
Michael Perez <mperez6473@gmail.com>
2020-09-03 22:42:58 -0500

    Adjusting single menu item by id

--- knexfile.js
+++ knexfile.js
@@ -2,61 +2,61 @@
 const pgConnection = process.env.DATABASE_URL;
 
 module.exports = {
-    development: {
-        client: "pg",
-        connection: {
-            host: "localhost",
-            database: "menu",
-            user: "postgres",
-            password: process.env.PASSWORD || "tH3w3Bc0D!ngP@s5c0D3",
-        },
-        migrations: {
-            directory: "./data/migrations",
-        },
-        seeds: {
-            directory: "./data/seeds",
-        },
+  development: {
+    client: "pg",
+    connection: {
+      host: "localhost",
+      database: "menu",
+      user: "postgres",
+      password: process.env.PASSWORD,
     },
-    testing: {
-        client: "pg",
-        connection: {
-            host: "localhost",
-            database: "menu",
-            user: "postgres",
-            password: process.env.PASSWORD || "tH3w3Bc0D!ngP@s5c0D3",
-        },
-        migrations: {
-            directory: "./data/migrations",
-        },
-        seeds: {
-            directory: "./data/testSeeds",
-        },
+    migrations: {
+      directory: "./data/migrations",
     },
+    seeds: {
+      directory: "./data/seeds",
+    },
+  },
+  testing: {
+    client: "pg",
+    connection: {
+      host: "localhost",
+      database: "menu",
+      user: "postgres",
+      password: process.env.PASSWORD,
+    },
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/testSeeds",
+    },
+  },
 
-    staging: {
-        client: "postgresql",
-        connection: {
-            database: "my_db",
-            user: "username",
-            password: "password",
-        },
-        pool: {
-            min: 2,
-            max: 10,
-        },
-        migrations: {
-            tableName: "knex_migrations",
-        },
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
     },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
 
-    production: {
-        client: "pg",
-        connection: pgConnection,
-        migrations: {
-            directory: "./data/migrations",
-        },
-        seeds: {
-            directory: "./data/seeds",
-        },
+  production: {
+    client: "pg",
+    connection: pgConnection,
+    migrations: {
+      directory: "./data/migrations",
     },
+    seeds: {
+      directory: "./data/seeds",
+    },
+  },
 };
--- menuItems/index.js
+++ menuItems/index.js
@@ -4,58 +4,58 @@ const router = express();
 const menuItems = require("./MenuItemsModel");
 //  Gets all the food items
 router.get("/", (req, res) => {
-    menuItems
-        .find()
-        .then((foodItems) => {
-            console.log(foodItems);
-            res.status(200).json({ foodItems });
-        })
-        .catch((err) => {
-            console.log(err);
-            res.status(400).json({ error: "error getting information" });
-        });
+  menuItems
+    .find()
+    .then((foodItems) => {
+      console.log(foodItems);
+      res.status(200).json({ foodItems });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json({ error: "error getting information" });
+    });
 });
 //  Gets the food items by category
 router.get("/categories", (req, res) => {
-    let filter = req.query.filter;
-    console.log(filter);
-    if (filter) {
-        return menuItems
-            .findItemsByCategories(filter)
-            .then((foodItems) => {
-                console.log(foodItems);
-                return res.status(200).json({ foodItems });
-            })
-            .catch((err) => {
-                console.log(err);
-                res.status(500).json(err);
-            });
-    } else {
-        return menuItems
-            .findCategories()
-            .then((categories) => {
-                // console.log(categories);
-                res.status(200).json({ categories });
-            })
-            .catch((err) => {
-                console.log(err);
-                res.status(400).json({
-                    error: "error getting information",
-                });
-            });
-    }
+  let filter = req.query.filter;
+  console.log(filter);
+  if (filter) {
+    return menuItems
+      .findItemsByCategories(filter)
+      .then((foodItems) => {
+        console.log(foodItems);
+        return res.status(200).json({ foodItems });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  } else {
+    return menuItems
+      .findCategories()
+      .then((categories) => {
+        // console.log(categories);
+        res.status(200).json({ categories });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json({
+          error: "error getting information",
+        });
+      });
+  }
 });
 //  Gets a food item by id
 router.get("/:id", (req, res) => {
-    menuItems
-        .findById(req.params.id)
-        .then((item) => {
-            console.log(item);
-            res.status(200).json(item);
-        })
-        .catch((err) => {
-            console.log(err);
-            res.status(404).json({ Error: "Item not found.", err });
-        });
+  menuItems
+    .findById(req.params.id)
+    .then((item) => {
+      console.log(item);
+      res.status(200).json(item);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(404).json({ Error: "Item not found.", err });
+    });
 });
 module.exports = router;
