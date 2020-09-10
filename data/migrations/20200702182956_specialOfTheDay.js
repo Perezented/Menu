20dcc9f95635c12e6cda92eecf1ee9cb1c469507
Michael Perez <mperez6473@gmail.com>
2020-09-10 09:22:59 -0500

    special of the day fix

--- data/migrations/20200702182956_specialOfTheDay.js
+++ data/migrations/20200702182956_specialOfTheDay.js
@@ -1,14 +1,16 @@
 exports.up = function (knex) {
-    return knex.schema.createTable("specialOfTheDay", (tbl) => {
-        tbl.increments();
-        tbl.string("sotdName").notNullable();
-        tbl.string("sotdDescription").notNullable();
-        tbl.float("sotdPrice").notNullable();
-        tbl.string("sotdAddtDescription");
-        tbl.float("sotdAddtPrice");
-    });
+  knex.schema.dropTableIfExists("specialOfTheDay");
+
+  return knex.schema.createTable("specialOfTheDay", (tbl) => {
+    tbl.increments();
+    tbl.string("sotdName").notNullable();
+    tbl.string("sotdDescription").notNullable();
+    tbl.float("sotdPrice").notNullable();
+    tbl.string("sotdAddtDescription");
+    tbl.float("sotdAddtPrice");
+  });
 };
 
 exports.down = function (knex) {
-    return knex.schema.dropTableIfExists("specialOfTheDay");
+  return knex.schema.dropTableIfExists("specialOfTheDay");
 };
