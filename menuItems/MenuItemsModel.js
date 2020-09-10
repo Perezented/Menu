49f51eb78df02d491ff5a01b1321304258057a44
Michael Perez <mperez6473@gmail.com>
2020-09-10 09:07:47 -0500

    Heroku needs refresh.

--- menuItems/MenuItemsModel.js
+++ menuItems/MenuItemsModel.js
@@ -1,43 +1,43 @@
 const db = require("../data/connection");
 module.exports = {
-    find,
-    findCategories,
-    findById,
-    findItemsByCategories,
-    // add,
-    // update,
-    // remove,
+  find,
+  findCategories,
+  findById,
+  findItemsByCategories,
+  // add,
+  // update,
+  // remove,
 };
 
 //  Gets menu items
 function find() {
-    return db("Menu").join(
-        //  Joins them with the category by menu item, category id
-        "Categories",
-        "Menu.categoryID",
-        "Categories.categoryID"
-    );
+  return db("Menu").join(
+    //  Joins them with the category by menu item, category id
+    "Categories",
+    "Menu.categoryID",
+    "Categories.categoryID"
+  );
 }
 //  Gets all the food categories
 function findCategories() {
-    return db("Categories").orderBy("category");
+  return db("Categories").orderBy("category");
 }
 //  Gets specified food by category
 function findItemsByCategories(filter) {
-    return (
-        db("Categories")
-            .orderBy("category")
-            //  join with menu
-            .join("Menu", "Categories.categoryID", "Menu.categoryID")
-            .where({ "Categories.category": filter })
-    );
+  return (
+    db("Categories")
+      .orderBy("category")
+      //  join with menu
+      .join("Menu", "Categories.categoryID", "Menu.categoryID")
+      .where({ "Categories.category": filter })
+  );
 }
 //  Gets a menu item by id
 function findById(id) {
-    return (
-        db("Menu")
-            .where("MenuItemID", id)
-            //  Joins with the category table
-            .join("Categories", "Menu.CategoryID", "Categories.CategoryID")
-    );
+  return (
+    db("Menu")
+      .where("MenuItemID", id)
+      //  Joins with the category table
+      .join("Categories", "Menu.CategoryID", "Categories.CategoryID")
+  );
 }
