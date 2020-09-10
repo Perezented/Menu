a6e85f79b59a07057a5b15fa2d42a83c856f5d43
Michael Perez <mperez6473@gmail.com>
2020-09-10 09:29:16 -0500

    the return of the special of the day table.

--- data/migrations/20200702182956_specialOfTheDay.js
+++ data/migrations/20200702182956_specialOfTheDay.js
@@ -1,14 +1,14 @@
 exports.up = function (knex) {
-  return knex.schema.dropTableIfExists("specialOfTheDay");
+  //   return knex.schema.dropTableIfExists("specialOfTheDay");
 
-  //   return knex.schema.createTable("specialOfTheDay", (tbl) => {
-  //     tbl.increments();
-  //     tbl.string("sotdName").notNullable();
-  //     tbl.string("sotdDescription").notNullable();
-  //     tbl.float("sotdPrice").notNullable();
-  //     tbl.string("sotdAddtDescription");
-  //     tbl.float("sotdAddtPrice");
-  //   });
+  return knex.schema.createTable("specialOfTheDay", (tbl) => {
+    tbl.increments();
+    tbl.string("sotdName").notNullable();
+    tbl.string("sotdDescription").notNullable();
+    tbl.float("sotdPrice").notNullable();
+    tbl.string("sotdAddtDescription");
+    tbl.float("sotdAddtPrice");
+  });
 };
 
 exports.down = function (knex) {
