a2bf173499569eacea29d0563a10bda3b6da26d5
Michael Perez <mperez6473@gmail.com>
2020-09-10 09:32:55 -0500

    Home slash message update.

--- server.js
+++ server.js
@@ -14,21 +14,21 @@ const specialOfTheDay = require("./specialOfTheDay/specialOfTheDay");
 const orderDetails = require("./orderDetalis/orderDetalis");
 //  Session configuration for knex
 const sessionConfig = {
-    name: "monster",
-    secret: process.env.SESSION_SECRET || "keep it secret, keep it safe!",
-    cookie: {
-        maxAge: 1000 * 600,
-        secure: process.env.COOKIE_SECURE || false, //  true means only use over https //  true in production
-        httpOnly: true, //JS code on the client cannot access the session cookie
-    }, // 10 min in milliseconds
-    resave: false,
-    saveUninitialiezed: false, //  GDPR compliance
-    store: new KnexSessionsStore({
-        knex: dbConnection,
-        sidfieldname: "sid",
-        createtable: true,
-        clearInterval: 30000, //  delete expired sessions - in milliseconds
-    }),
+  name: "monster",
+  secret: process.env.SESSION_SECRET || "keep it secret, keep it safe!",
+  cookie: {
+    maxAge: 1000 * 600,
+    secure: process.env.COOKIE_SECURE || false, //  true means only use over https //  true in production
+    httpOnly: true, //JS code on the client cannot access the session cookie
+  }, // 10 min in milliseconds
+  resave: false,
+  saveUninitialiezed: false, //  GDPR compliance
+  store: new KnexSessionsStore({
+    knex: dbConnection,
+    sidfieldname: "sid",
+    createtable: true,
+    clearInterval: 30000, //  delete expired sessions - in milliseconds
+  }),
 };
 //  make sure to use the sessionConfig
 server.use(session(sessionConfig));
@@ -39,10 +39,11 @@ server.use(express.json());
 
 //  Actual endpoints for the server
 server.get("/", (req, res) => {
-    res.status(200).json({
-        Message: "Welcome to the home slash of the menu server.",
-        menuItems,
-    });
+  res.status(200).json({
+    Message:
+      "Welcome to the home slash of the menu server. You may be looking for https://node-menu.herokuapp.com/menu",
+    menuItems,
+  });
 });
 //  custom routes
 server.use("/menu", menuItems);
