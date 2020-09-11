ddf7cf199b4216350ff2b3a1659c59617b115a7a
Michael Perez <mperez6473@gmail.com>
2020-09-11 02:45:32 -0500

    Fixed table so front end can send orders in.

--- data/migrations/20200716111334_cart.js
+++ data/migrations/20200716111334_cart.js
@@ -1,18 +1,19 @@
 exports.up = function (knex) {
-    return knex.schema.createTable("cart", (tbl) => {
-        tbl.increments();
-        tbl.float("total").notNullable();
-        tbl.integer("OrderID").notNullable();
-        tbl.integer("menuItemID")
-            .unsigned()
-            .references("Menu.menuItemID")
-            .onDelete("RESTRICT")
-            .onUpdate("CASCADE");
-        tbl.integer("quantity").notNullable();
-        tbl.timestamp("created_at", { useTz: true });
-    });
+  return knex.schema.createTable("cart", (tbl) => {
+    tbl.increments();
+    tbl.float("total").notNullable();
+    tbl.string("orderID").notNullable();
+    tbl
+      .integer("menuItemID")
+      .unsigned()
+      .references("Menu.menuItemID")
+      .onDelete("RESTRICT")
+      .onUpdate("CASCADE");
+    tbl.integer("quantity").notNullable();
+    tbl.timestamp("created_at", { useTz: true });
+  });
 };
 
 exports.down = function (knex) {
-    return knex.schema.dropTableIfExists("cart");
+  return knex.schema.dropTableIfExists("cart");
 };
