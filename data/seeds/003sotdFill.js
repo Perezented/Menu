f9b72d7e888cb9af59ba9cfcf726a96159ee77b5
Michael Perez <mperez6473@gmail.com>
2020-10-30 12:50:25 -0500

    Updated the special of the day with realistic data

--- data/seeds/003sotdFill.js
+++ data/seeds/003sotdFill.js
@@ -1,18 +1,18 @@
 exports.seed = function (knex) {
-    // Deletes ALL existing entries
-    return knex("specialOfTheDay")
-        .del()
-        .then(function () {
-            // Inserts seed entries
-            return knex("specialOfTheDay").insert([
-                {
-                    sotdName: "FoodName",
-                    sotdDescription:
-                        "With a description to describe what is going on.",
-                    sotdPrice: 9.99,
-                    sotdAddtDescription: "With extra cheese or something",
-                    sotdAddtPrice: 1.99,
-                },
-            ]);
-        });
+  // Deletes ALL existing entries
+  return knex("specialOfTheDay")
+    .del()
+    .then(function () {
+      // Inserts seed entries
+      return knex("specialOfTheDay").insert([
+        {
+          sotdName: "Pollo Asado con arroz y frijoles",
+          sotdDescription:
+            "Grilled chicken that has been marinated for 24 hours in our special tapatillo sauce, servered with rice and beans on the side.",
+          sotdPrice: 8.99,
+          sotdAddtDescription: "Extra chicken or add shrimp",
+          sotdAddtPrice: 1.99
+        }
+      ]);
+    });
 };
